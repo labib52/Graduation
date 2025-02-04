e7b628fbf021b080b4ae497518c1945b8f5ce680
youssef sherin <125735335+sherry259@users.noreply.github.com>
2025-02-04 19:11:27 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -9,7 +9,7 @@ $dbname = "newgrad"; // Replace with your database name
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
