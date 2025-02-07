101de3a7d8339378e97bb50ee961971c95666509
mohamedalyhussien <112475248+mohamedalyhussien@users.noreply.github.com>
2025-02-07 22:50:17 +0400

    mo

--- attack1networklec.php
+++ attack1networklec.php
@@ -20,7 +20,7 @@
             padding: 20px;
         }
 
-        h1, h2 {
+        h1, h2 ,h3,h4,h5{
             color: #007BFF;
         }
 
@@ -33,6 +33,7 @@
             font-size: 1.5rem;
             margin-top: 20px;
         }
+        
 
         p {
             margin: 10px 0;
@@ -100,118 +101,66 @@
 </head>
 <body>
     <div class="container">
-        <h1>Understanding Port Scanning Attacks: An Overview</h1>
-        <p>Port scanning is a technique used by attackers to identify open ports and services available on a networked device. The primary goal of port scanning is to find potential vulnerabilities that can be exploited for malicious purposes, such as unauthorized access, data theft, or spreading malware. Port scanning is often the first step in a broader cyberattack, as attackers need to gather information about the target system to exploit its weaknesses.</p>
+        <h1>Challenge Description</h1>
+        <p>One of the clients of our company, lost the access to his system due to an unknown error. He is supposedly a very popular “environmental” activist. As a part of the investigation, he told us that his go to applications are browsers, his password managers etc. We hope that you can dig into this memory dump and find his important stuff and give it back to us.</p>
+        <h3>Volatility installer :</h3>
+        <p><b>Note: </b>First of all, you need to install <a href="https://letsdefend.io/blog/how-to-install-volatility-2-and-volatility-3-on-linux">Volatility 2</a></p>
 
-        <h2>How Port Scanning Works</h2>
-        <p>
-        Port scanning involves sending requests to specific ports on a target machine and analyzing the responses to determine whether the ports are open, closed, or filtered. Each service on a computer or network device (such as HTTP, FTP, or SSH) listens for incoming connections on a specific port number. By scanning ports, attackers can map out which services are running and then attempt to exploit any known vulnerabilities associated with those services.
 
-There are different methods of port scanning, each with its own characteristics and detection rates:
-        </p>
-        <ul>
-            <li><strong>TCP Connect Scan (Full Open Scan): </strong> The attacker attempts to establish a full TCP connection with the target machine on each port. If the connection is successful, the port is open.
-            <ul><li><strong>Detection:</strong>This type of scan is the most easily detected because it completes the handshake process with the target system, which can trigger logs and alerts.</li></ul></li>
-            <li><strong>SYN Scan (Half-Open Scan): </strong> The attacker sends a SYN packet to the target port, simulating the start of a TCP connection. If the port is open, the target responds with a SYN-ACK packet. The attacker then sends a RST (reset) packet to close the connection before it is fully established.
-            <ul><li><strong>Detection:</strong>This scan is stealthier than the TCP connect scan since it does not complete the handshake and is harder to detect.</li></ul></li>
-            <li><strong>UDP Scan: </strong> The attacker sends a UDP packet to the target port. If the port is open, the target may respond with a message indicating that the service is running. If the port is closed, the target will typically respond with an ICMP "port unreachable" message.
-            <ul><li><strong>Detection:</strong>UDP scans are less common but are often used when an attacker suspects that the target is running non-TCP services. Detection is more difficult because the absence of a response (for an open port) does not necessarily indicate anything suspicious.</li></ul></li>
-            <li><strong>FIN Scan: </strong> The attacker sends a FIN packet, which is normally used to terminate an open connection. If the target port is open, it will ignore the FIN packet, but if the port is closed, the system will respond with an RST (reset) packet.
-            <ul><li><strong>Detection:</strong>This is a stealthier scan type because it doesn’t establish a connection, but it can be detected through anomalies in system logs.</li></ul></li>
-            <li><strong>Xmas Scan: </strong>The attacker sends a packet with the FIN, URG, and PSH flags set, causing the packet to appear as though it’s part of an abnormal or "Christmas tree" configuration.
-            <ul><li><strong>Detection:</strong>Like the FIN scan, it can be used for stealth scanning but can trigger unusual system behavior and is detectable through network monitoring tools.</li></ul></li>
-            <li><strong>Stealth Scanning: </strong> The attacker uses techniques like fragmenting packets or using decoy IP addresses to obscure the origin of the scan and avoid detection. This is often done to bypass firewalls or intrusion detection systems.</li>
-        </ul>
+<p>This challenge is composed of 3 flags.</p>
+<p><b>Challenge file:</b><a href="https://mega.nz/#!ChoDHaja!1XvuQd49c7-7kgJvPXIEAst-NXi8L3ggwienE1uoZTk">MemLabs Lab2</a></p>
+</div>
+<div class="container">
+    <h2>First Flag :</h2>
+        <h3>First we need to identify the operating system of the memory image.</h3>
+        <p><b>  $ volatility -f MemoryDump_Lab2.raw imageinfo</b></p>
+        <h3>Next, let’s check the processes list.</h3>
+        <p><b>  $ volatility -f MemoryDump_Lab2.raw --profile Win7SP1x64 pslist</b></p>
+        <h3>We can see interesting processes like chrome and KeePass. but first let’s look back at the description, note the quoted word "environmental". I think it’s a hint for environment variables, so let’s go down this way first.</h3>
+        <p><b>  $ volatility -f MemoryDump_Lab2.raw --profile Win7SP1x64 envars</b></p>
+        <h3>We can see the environment variable NEW_TMP in every process with a value that looks like Base64. so let’s decode it.</h3>
+        <p><b>  $ echo ZmxhZ3t3M2xjMG0zX1QwXyRUNGczXyFfT2ZfTDRCXzJ9 | base64 -d</b></p>
+        <br>
+        <p><b>Result : flag{w3lc0m3_T0_$T4g3_!_Of_L4B_2}</b></p>
+        <p><b style="color: red;">Great, first stage is done.</b></p>
+    </div>
+    <div class="container">
+        <h2>Second Flag : </h2>
+        <h3>Next, let’s check this KeePass process, looks like a password manager.</h3>
+        <p>After some googling, I learned that KeePass stores the passwords in a database with the extension ".kdbx" and looks it with a master password</p>
+        <p>So let’s check if this database is in memory.</p>
+        <p><b>$ volatility -f MemoryDump_Lab2.raw --profile Win7SP1x64 filescan | grep ".kdbx"</b></p>
+        <p>And here it’s, now let’s dump it.</p>
+        <p><b>$ volatility -f MemoryDump_Lab2.raw --profile Win7SP1x64 dumpfiles -Q 0x000000003fb112a0 -D lab2_output/</b></p>
+        <p>The only thing left is to get the master password, I tried scanning files for any password like file</p>
+        <p><b>$ volatility -f MemoryDump_Lab2.raw --profile Win7SP1x64 filescan | grep -i "password"</b></p>
+        <p>Look at that, an image named Password!!! looks interesting, let’s dump it.</p>
+        <p><b>$ volatility -f MemoryDump_Lab2.raw --profile Win7SP1x64 dumpfiles -Q 0x000000003fce1c70 -D lab2_output/</b></p>
+        <br>
+        <p><b>Note: </b>If you look closely at the bottom right, you can spot the password.</p>
+        <img src="public/images/flag2.png" alt="">
 
-        <h2>Recognizing Port Scanning Attempts</h2>
-        <p>Port scanning can sometimes be difficult to detect, but there are several signs that indicate an attack is taking place:</p>
-        <ul>
-            <li><strong>Increased Unusual Traffic: </strong> A large volume of connection attempts to various ports within a short period of time might indicate that a port scan is happening.</li>
-            <li><strong>Suspicious Connections from Unknown IPs: </strong>Connections coming from unfamiliar or external sources to multiple ports are often a sign of a port scanning activity.</li>
-            <li><strong>Repeated Access Attempts to Specific Ports: </strong>If there are multiple connection attempts to a particular service or application port (such as port 22 for SSH or 80 for HTTP), it could indicate an attacker is trying to gain access through that specific service.</li>
-            <li><strong>Intrusion Detection System (IDS) Alerts: </strong>IDS and firewalls are often configured to detect port scanning attempts. Anomalous patterns in traffic, such as rapid connections to multiple ports, can trigger alarms.</li>
-        </ul>
-        <h2>How to Protect Against Port Scanning Attacks</h2>
-        <ol>
-            <li>
-                <strong>Use Firewalls: </strong> 
-                    <ul><li>A properly configured firewall can block unnecessary ports and restrict access to certain ports from untrusted IP addresses, thereby reducing the attack surface.</li>
-                     </ul>
-            </li>
+        <p><b>Now let’s use this password to open the database in KeePass.</b></p>
+        <img src="public/images/flag22.png" alt=""><img src="public/images/flag222.png" alt="">
+        <br>
+        <p><b style="color: red;">Flag 2: flag{w0w_th1s_1s_Th3SeC0nD_ST4g3!!}</b></p>
+    </div>
 
-            
-            <li>
-                <strong>Intrusion Detection and Prevention Systems (IDPS):</strong> 
-                    <ul>
-                        <li>IDS/IPS systems can detect abnormal port scanning patterns and take preventive actions, such as blocking the source IP of the scan or alerting administrators.</li>
-                     </ul>
-            </li>
+    <div class="container">
+<h2>Third Flag: </h2>
+<h3>Now let’s return back the the chrome process, the first thing is to check the browsing history.</h3>
+<p><b>volatility --plugins=plugins/ -f MemoryDump_Lab2.raw --profile Win7SP1x64 chromehistory > chromehistory.txt</b></p>
+<br>
+<p>We have a mega link, the mega folder name is MemLabs_Lab2_Stage3 and it contained a single zip file named <b>Important.zip</b> (password protected).</p>
+<br>
+<p>I tried unzipping it with <b>unzip</b> but it gave me an error, so I used <b>7z</b>.</p>
+<br>
+<h4>Let’s get the password.</h4>
+<p><b>$ echo -n flag{w3ll_3rd_stage_was_easy} | sha1sum</b></p>
+<h4>After unzipping the file, I got this image.</h4>
+<img src="public/images/flag3.png" alt="">
 
-            <li>
-                <strong>Disable Unused Ports: </strong> 
-                    <ul>
-                        <li>Disable or close any ports on your network or device that are not being used for essential services. Unused ports provide attackers with more entry points.</li>
-                     </ul>
-            </li>
-
-            <li>
-                <strong>Port Knocking: </strong> 
-                    <ul>
-                        <li>This technique involves using a sequence of "knocks" (specific port requests) to trigger a firewall to temporarily open a port for legitimate users. It makes it harder for attackers to discover open ports without knowing the specific sequence.</li>
-                     </ul>
-            </li>
-
-            <li>
-                <strong>Conduct Regular Security Audits: </strong> 
-                    <ul>
-                        <li>Regular vulnerability scans and penetration testing help identify open ports and services that should be closed or hardened against attacks.</li>
-                     </ul>
-            </li>
-
-            <li>
-                <strong>Use VPNs and Secure Connections: </strong> 
-                    <ul>
-                        <li>For sensitive services, require a Virtual Private Network (VPN) or other secure connections to access the system. This ensures that only authorized users can interact with the network’s services.</li>
-                     </ul>
-            </li>
-
-            <li>
-                <strong>Implement Rate Limiting: </strong> 
-                    <ul>
-                        <li>Limiting the number of connection attempts a user can make in a given time frame can help prevent brute-force port scanning attacks.</li>
-                     </ul>
-            </li>
-             </ol>
-             
-
-        <h2>Consequences of Port Scanning Attacks</h2>
-        <ul>
-            <li><strong>Unauthorized Access: </strong> The attacker may identify a vulnerable service and attempt to exploit it to gain unauthorized access to the system or network.</li>
-            <li><strong>Security Breach: </strong> If an attacker successfully finds and exploits an open port, they could compromise data integrity, steal sensitive information, or inject malware into the system.</li>
-            <li><strong>Legal and Compliance Issues: </strong> Once the attacker knows which ports are open, they may launch a denial of service attack to overload or disable the service.</li>
-            <li><strong>Reputation Damage: </strong> If an organization is successfully attacked through a vulnerable port, it may suffer reputational damage, loss of customer trust, and legal consequences.</li>
-        </ul>
-        <?php
-    $imagePath = "port.jpg"; // Replace with the path to your image
-    echo "<img src='$imagePath' alt='Image'>";
-    ?>
-        <h2>Conclusion</h2>
-        <p>
-        Port scanning is a critical step in the reconnaissance phase of a cyberattack. By identifying open ports, attackers can potentially exploit weaknesses in the services running on those ports. However, organizations can defend against port scanning through a combination of proactive security measures, such as firewalls, intrusion detection systems, and regular vulnerability assessments. Awareness and preparedness are key to mitigating the risk of port scanning and protecting network infrastructure.
-        </p>
-
-        <div class="navigation">
-            <a href="network.php" class="nav-btn">
-                <span class="arrow arrow-back">&larr;</span>
-                <span>Back</span>
-            </a>
-            <a href="attack1networklab.php" class="nav-btn">
-                <span>Next</span>
-                <span class="arrow arrow-next">&rarr;</span>
-            </a>
-        </div>
+<br><p><b style="color:red">Flag 3: flag{oK_So_Now_St4g3_3_is_DoNE!!}</b></p>
     </div>
 </body>
 </html>
-
-
