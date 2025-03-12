404a41ab91d0a87c0de36de19ba309144fada461
youssef sherin <125735335+sherry259@users.noreply.github.com>
2025-03-12 22:40:22 +0200

    Update view_profile.php

--- views/view_profile.php
+++ views/view_profile.php
@@ -89,8 +89,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/CSS/View_profile.css">
     <title>View Profile</title>
-    <link rel="stylesheet" href="../public/CSS/view_profile.css">
 
 </head>
 <body>
