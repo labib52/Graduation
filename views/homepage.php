c605885e878ec85176e18ef6a654db63681c34a3
mohamedalyhussien <112475248+mohamedalyhussien@users.noreply.github.com>
2025-03-30 17:42:28 +0200

    moo

--- views/homepage.php
+++ views/homepage.php
@@ -94,6 +94,19 @@ if ($loggedIn) {
 
     <footer class="footer">
         &copy; <?php echo date("Y"); ?> Cybersecurity Training Platform | All Rights Reserved.
+       <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67e95bf057b42a191471b8d5/1injou94l';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
     </footer>
 </body>
 </html>
