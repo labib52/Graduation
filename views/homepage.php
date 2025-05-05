24399799b44434896cba9a65796b81a3d7c26295
youssef sherin <125735335+sherry259@users.noreply.github.com>
2025-05-05 20:24:23 +0300

    Update homepage.php

--- views/homepage.php
+++ views/homepage.php
@@ -81,19 +81,7 @@ if ($loggedIn) {
 
 <!DOCTYPE html>
 <html lang="<?php echo $lang; ?>" dir="<?php echo $lang === 'ar' ? 'rtl' : 'ltr'; ?>">
-<div id="google_translate_element"></div>
 
-<script type="text/javascript">
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement({
-                pageLanguage: 'en'
-            },
-            'google_translate_element'
-        );
-    }
-</script>
-
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
 <head>
     <meta charset="UTF-8">
