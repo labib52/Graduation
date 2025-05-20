8c4f26f0a819a2ccdaae95524b2108af0c3a063d
Robert221102 <135883452+Robert221102@users.noreply.github.com>
2025-05-20 20:17:38 +0300

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'newgrad'); // or your actual database name
+define('DB_USER', 'root');    // or your actual DB username
+define('DB_PASS', '');        // or your actual DB password
+?>
