24d034b855abd2fc19dc09fcb77ab26050a46534
youssef sherin <125735335+sherry259@users.noreply.github.com>
2025-05-25 23:48:37 +0300

    Update admin_edit_lab.php

--- views/admin_edit_lab.php
+++ views/admin_edit_lab.php
@@ -114,7 +114,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Lab</title>
-    <link rel="stylesheet" href="../public/CSS/admin_styles.css">
+    <link rel="stylesheet" href="../public/CSS/admin_styles_1.css">
     <style>
         .question-group {
             background: #f8f9fa;
