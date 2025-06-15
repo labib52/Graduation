81efcd20c5408d1bd9b8e168e36780c364c38d6e
youssef sherin <125735335+sherry259@users.noreply.github.com>
2025-06-15 18:03:31 +0300

    Update attack_networksniffing.php

--- views/attack_networksniffing.php
+++ views/attack_networksniffing.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wireless Security Simulation</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../public/CSS/network_2.css">
+    <link rel="stylesheet" href="../public/CSS/reflectedxss_1.css">
 </head>
 <body>
     <header>
